185d907ed8e3edb226500e04420c5e4c61131a54
paulcruz-dev <johnpaulcruz859@gmail.com>
2025-08-27 21:48:43 +0800

    feat: add Part 2 starter files and documentation

---
+++ 02-fundamentals-part-2/starter/script.js
@@ -0,0 +1,235 @@
+// console.log("Part 2: Functions ready!");
+ 
+// // // Functions - Declarations and Expressions
+// // function logger() {
+// //     console.log("My name is Paul");
+// // }
+ 
+// // logger();
+// // logger();
+// // logger();
+ 
+// // function fruitProcessor(apples, oranges) {
+// //   console.log(apples, oranges);
+// //   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+// //   return juice;
+// // }
+ 
+// // console.log(fruitProcessor(5, 3));
+ 
+// // const juice1 = fruitProcessor(5, 0);
+// // const juice2 = fruitProcessor(2, 4);
+// // const juice3 = fruitProcessor(3, 2);
+ 
+// // // Function Expressions
+// // const calcAge = function(birthYear) {
+// //     return 2037 - birthYear;
+// // };
+ 
+// // const age1 = calcAge(1991);
+// // console.log(age1);
+ 
+// // console.log(calcAge(1991));
+ 
+// // function introduce(firstName, lastName, age) {
+// //     const introduction = `Hi, I am ${firstName} ${lastName} and I am ${age} years old.`;
+// //     return introduction;
+// // }
+ 
+// // console.log(introduce("Paul", "Cruz", 22));
+// // console.log(introduce("Sarah"));
+ 
+// // function yearsUntilRetirement(birthYear, firstName) {
+// //     const age = calcAge(birthYear);
+// //     const retirement = 65 - age;
+ 
+// //     if(retirement > 0) {
+// //         return `${firstName} retires in ${retirement} years`;
+// //     } else {
+// //         return `${firstName} has already retired!`;
+// //     }
+// // }
+ 
+// // console.log(yearsUntilRetirement(1991, "Paul"));
+ 
+// // // Function Scope
+// // const globalVar = "I am global";
+ 
+// // function testScope() {
+// //     const localVar = "I am local";
+// //     console.log(globalVar);
+// //     console.log(localVar);
+// // }
+ 
+// // testScope();
+// // console.log(globalVar);
+// // // console.log(localbar);
+ 
+// // // THE CHALLENGE
+// // function calcAverage(score1, score2, score3) {
+// //   return (score1 + score2 + score3) / 3;
+// // }
+ 
+// // // Function to check winner
+// // function checkWinner(avgDolphins, avgKoalas) {
+// //     if (avgDolphins >= 2 * avgKoalas) {
+// //         console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+// //     } else if (avgKoalas >= 2 * avgDolphins) {
+// //         console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+// //     } else {
+// //         console.log(`No team wins`);
+// //     }
+// // }
+// // // Test Data 1
+// // let scoreDolphins = calcAverage(44, 23, 71);
+// // let scoreKoalas = calcAverage(65, 54, 49);
+// // console.log(checkWinner(scoreDolphins, scoreKoalas));
+ 
+// // // Test Data 2
+// // scoreDolphins = calcAverage(85, 54, 41);
+// // scoreKoalas = calcAverage(23, 34, 27);
+// // console.log(checkWinner(scoreDolphins, scoreKoalas));
+
+// const grades = [85, 92, 78, 96, 88];
+// console.log(grades);
+ 
+// const friends = ["John", "Paul", "Mark"];
+// console.log(friends);
+ 
+// const mixed = ["Jeyp", 24, false, friends];
+// console.log(mixed);
+ 
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
+ 
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+// console.log(friends[3]);
+ 
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
+ 
+// friends[0] = "Jay";
+// console.log(friends);
+ 
+// const calcAge = function(birthYear) {
+//     return 2037 - birthYear;
+// };
+ 
+// const ages = [calcAge(1991), calcAge(1967), calcAge(2002)];
+// console.log(ages);
+ 
+// const newLength = friends.push("Tom");
+// console.log(friends);
+// console.log(newLength);
+ 
+// friends.unshift("John");
+// console.log(friends);
+ 
+// // Removing Element
+// // Last Item
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+// // First Item
+// const shifted = friends.shift();
+// console.log(shifted);
+// console.log(friends);
+ 
+// console.log(friends.indexOf("Paul"));
+// console.log(friends.indexOf("Mark"));
+ 
+// console.log(friends.includes("Paul"));
+// console.log(friends.includes("John"));
+ 
+// // Array Iteration
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(friends[i]);
+// }
+ 
+// friends.forEach(
+//     function(friends, index) {
+//         console.log(`${index + 1}: ${friends}`);
+//     }
+// );
+ 
+// friends.forEach(
+//     (friend, index) => {
+//         console.log(`Friend ${index + 1}: ${friend}`);
+//     }
+// )
+ 
+// const grades2 = [85, 92, 78, 96, 88, 74];
+// let total = 0;
+ 
+// for (let i = 0; i < grades2.length; i++) {
+//   total += grades2[i];
+// }
+ 
+// const average1 = total / grades2.length;
+// console.log(`Average grade: ${average.toFixed(2)}`);
+ 
+// let passedCount = 0;
+// grades2.forEach((grade) => {
+//   if (grade >= 70) passedCount++;
+// });
+// console.log(`${passedCount} out of ${grades2.length} students passed`);
+ 
+// Coding Challenge #2 - Student Grade Manager
+ 
+const grades3 = [78, 85, 92, 67, 88, 95, 73, 82];
+
+// Function to calculate average
+function calculateAverage(grades) {
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+        sum += grades[i];
+    }
+    return sum / grades.length;
+}
+
+// Function to find highest grade
+function findHighestGrade(grades) {
+    let highest = grades[0];
+    for (let i = 1; i < grades.length; i++) {
+        if (grades[i] > highest) {
+            highest = grades[i];
+        }
+    }
+    return highest;
+}
+
+// Function to find lowest grade
+function findLowestGrade(grades) {
+    let lowest = grades[0];
+    for (let i = 1; i < grades.length; i++) {
+        if (grades[i] < lowest) {
+            lowest = grades[i];
+        }
+    }
+    return lowest;
+}
+
+// Function to count passing students
+function countPassing(grades, passingGrade) {
+    let count = 0;
+    for (let i = 0; i < grades.length; i++) {
+        if (grades[i] >= passingGrade) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Generate complete report
+const average = calculateAverage(grades3);
+const highest = findHighestGrade(grades3);
+const lowest = findLowestGrade(grades3);
+const passing = countPassing(grades3, 70);
+
+console.log("=== GRADE REPORT ===");
+console.log(`Average: ${average.toFixed(2)}`);
+console.log(`Highest: ${highest}`);
+console.log(`Lowest: ${lowest}`);
+console.log(`Passing students: ${passing} out of ${grades3.length}`);
